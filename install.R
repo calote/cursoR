f95b06e8ccc8b895a4252dde3317c51c84300604
Pedro Luis Luque Calvo <pedro.luis.luque@gmail.com>
2019-10-01 14:23:37 +0200

    Cambios: rmdshower, rmdTemplates

--- install.R
+++ install.R
@@ -2,17 +2,14 @@ install.packages("tidyverse")
 install.packages("rmarkdown")
 install.packages("httr")
 install.packages("shinydashboard")
-install.packages(c('leaflet'))
+install.packages(c('leaflet','flexdashboard'))
 install.packages(c('kableExtra','huxtable','xtable','gt','texreg','flextable','rhandsontable'))
+
 install.packages(c("rio","fBasics","skimr"))
 
-install.packages(c("fractional","bookdown","rticles",
-                   "rmdshower","printr","rmdTemplates",
-                   "flextable","officer","xaringan",
-                   "openxlsx","eurostat","DT",))
+install.packages(c("fractional","bookdown","rticles","printr","officer","xaringan","openxlsx","eurostat","DT"))
 
-install.packages(c("hflights","nycflights13","sparklyr",
-                   "RSQLite","stargazer","reticulate"))
+install.packages(c("hflights","nycflights13","sparklyr","RSQLite","stargazer","reticulate"))
 
 install.packages(c("Rglpk","lpSolve"))
 
@@ -21,4 +18,6 @@ library(devtools)
 install_github("rapporter/pander") # Necessary for optimal results!
 install_github("dcomtois/summarytools")
 
-##'flexdashboard'
+##
+devtools::install_github("mangothecat/rmdshower")
+install_github("Pakillo/rmdTemplates")
